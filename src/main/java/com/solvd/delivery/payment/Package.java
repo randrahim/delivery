c97a487ac808d52a09dfa4a7ff992fc920ccc37e
Rand Rahim <48077869+randrahim@users.noreply.github.com>
2023-02-01 09:51:45 -0500

    Update Package.java

--- src/main/java/com/solvd/delivery/payment/Package.java
+++ src/main/java/com/solvd/delivery/payment/Package.java
@@ -1,44 +1,64 @@
-package com.solvd.delivery.payment;
+package com.solvd.delivery;
 
-public class Package {
+import com.solvd.delivery.interfaces.ICart;
+import com.solvd.delivery.interfaces.ITransportation;
+import com.solvd.delivery.utils.Generator;
+import com.solvd.delivery.utils.PrintValues;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-    private int packageSerialNumber;
-    private double packageWeight;
+import static com.solvd.delivery.utils.Generator.generatorProduct;
 
-    // ------------------------ Constructors -------------------------//
+public class Main extends Thread {
+    private static Logger logger = LogManager.getLogger(Main.class);
+    public static void main(String[] args) {
 
-    public Package() { }
+        // Using PrintValues to print out the values from Generator class
+        Generator generator = new Generator();
+        PrintValues printValues = new PrintValues();
 
-    public Package(int packageSerialNumber) {
-        this.packageSerialNumber = packageSerialNumber;
-    }
+        printValues.printManager();
+        printValues.printEmployee();
+        printValues.printCustomer();
 
-    public Package(double packageWeight) {
-        this.packageWeight = packageWeight;
-    }
+        printValues.printProduct();
+        printValues.printPackage();
+        printValues.printNotifications();
+        printValues.printEnums();
 
-    public Package(int packageSerialNumber, double packageWeight) {
-        this.packageSerialNumber = packageSerialNumber;
-        this.packageWeight = packageWeight;
-    }
+        double thePackageWeight = generator.generatorPackage().getPackageWeight();
+        System.out.println("The package weight is: " + thePackageWeight);
 
-    // ------------------------ Getters -------------------------//
+        // Using Lambda Expression & Functional Interface to get transportation way to ship the package
+        // Decide what Transportation Way (Truck, Train, Ship, Airplane) based on the PackageWeight
+        ITransportation iTransportation = () -> {
+            String transportation = "";
+            if ((0 <= thePackageWeight) && (thePackageWeight <= 10)) {
+                transportation = "Truck";
+                printValues.printTruck();
+            } else if ((11 <= thePackageWeight) && (thePackageWeight <= 20)) {
+                transportation = "Train";
+                printValues.printTrain();
+            } else if ((21 <= thePackageWeight) && (thePackageWeight <= 30)) {
+                transportation = "The transportation way is Ship";
+                printValues.printShip();
+            } else if (31 <= thePackageWeight) {
+                transportation = "Airplane";
+                printValues.printAirplane();
+            }
+            return transportation;
+        };
 
-    public int getPackageSerialNumber() {
-        return packageSerialNumber;
-    }
+        logger.error(iTransportation.transWay());
 
-    public double getPackageWeight() {
-        return packageWeight;
-    }
+        // Using Lambda Expression & Functional Interface to get the Total Payment
+        double theItemPrice = generatorProduct().getPrice();
+        int quantity = generator.generatorProduct().getQuantity();
 
-    // ------------------------ Setters -------------------------//
-
-    public void setPackageSerialNumber(int packageSerialNumber) {
-        this.packageSerialNumber = packageSerialNumber;
-    }
-
-    public void setPackageWeight(double packageWeight) {
-        this.packageWeight = packageWeight;
+        ICart iCart = () -> {
+            double total = (theItemPrice + (theItemPrice * 0.06)) * quantity;
+            return total;
+        };
+        logger.error("The total payment is: " + iCart.totalPayment());
     }
 }
