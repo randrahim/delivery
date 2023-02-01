50d5e42bbb0bb86d1302f46ea945cdd4098d6a05
Rand Rahim <48077869+randrahim@users.noreply.github.com>
2023-02-01 09:59:55 -0500

    Update Employee.java

--- src/main/java/com/solvd/delivery/people/Employee.java
+++ src/main/java/com/solvd/delivery/people/Employee.java
@@ -1,9 +1,15 @@
 package com.solvd.delivery.people;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Objects;
+
 public class Employee extends Person {
     private int employeeID;
     private String employeePosition;
     private double employeeSalary;
+    private static final Logger logger = LogManager.getLogger(Employee.class);
 
     // ------------------------ Constructors -------------------------//
     public Employee() {
@@ -45,4 +51,27 @@ public class Employee extends Person {
         return employeeSalary;
     }
 
+    // ------------------------ Add Override equals(), hashCode(), toString() -------------------------//
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Employee)) return false;
+        Employee employee = (Employee) o;
+        return getEmployeeID() == employee.getEmployeeID() && Double.compare(employee.getEmployeeSalary(), getEmployeeSalary()) == 0 && getEmployeePosition().equals(employee.getEmployeePosition());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getEmployeeID(), getEmployeePosition(), getEmployeeSalary());
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeID=" + employeeID +
+                ", employeePosition='" + employeePosition + '\'' +
+                ", employeeSalary=" + employeeSalary +
+                '}';
+    }
 }
