8ce6ceb03891708514053a1de0f6fb30e50c16fd
Rand Rahim <48077869+randrahim@users.noreply.github.com>
2023-02-01 10:05:32 -0500

    Update Main.java

--- src/main/java/com/solvd/delivery/Main.java
+++ src/main/java/com/solvd/delivery/Main.java
@@ -1,97 +1,64 @@
 package com.solvd.delivery;
 
-import com.solvd.delivery.lambda.Discount;
-import com.solvd.delivery.lambda.InsuranceRate;
-import com.solvd.delivery.lambda.Places;
-import com.solvd.delivery.payment.Notifications;
-import com.solvd.delivery.payment.Payment;
-import com.solvd.delivery.payment.Transportation;
-import com.solvd.delivery.people.Generator;
+import com.solvd.delivery.interfaces.ICart;
+import com.solvd.delivery.interfaces.ITransportation;
+import com.solvd.delivery.utils.Generator;
+import com.solvd.delivery.utils.PrintValues;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import java.util.List;
+import static com.solvd.delivery.utils.Generator.generatorProduct;
 
-public class Main {
+public class Main extends Thread {
+    private static Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
 
-//  Get information from Generator --------------------
-    Generator generator = new Generator();
-    Payment payment = new Payment();
-    System.out.println("\n");
+        // Using PrintValues to print out the values from Generator class
+        Generator generator = new Generator();
+        PrintValues printValues = new PrintValues();
 
-System.out.println("----------------- Employee information -----------------------------------");
-        System.out.println("Employee ID: " + generator.generatorEmployee().getEmployeeID());
-        System.out.println("Employee Full Name: " + generator.generatorEmployee().getFullName());
-        System.out.println("Employee Address: " + generator.generatorEmployee().getAddress());
-        System.out.println("Employee Email: " + generator.generatorEmployee().getEmail());
-        System.out.println("Employee Position: " + generator.generatorEmployee().getEmployeePosition());
-        System.out.println("Employee Salary: " + generator.generatorEmployee().getEmployeeSalary() + "\n");
+        printValues.printManager();
+        printValues.printEmployee();
+        printValues.printCustomer();
 
-System.out.println("----------------- Customer information -----------------------------------");
-     System.out.println("Customer ID: " + generator.generatorCustomer().getCustomerID());
-     System.out.println("Customer Full Name: " + generator.generatorCustomer().getFullName());
-     System.out.println("Customer Address: " + generator.generatorCustomer().getAddress());
-     System.out.println("Customer Email: " + generator.generatorCustomer().getEmail());
-     boolean customerIsMember = generator.generatorCustomer().isMember();
-     System.out.println("Customer Membership: " + generator.generatorCustomer().isMember() + "\n");
+        printValues.printProduct();
+        printValues.printPackage();
+        printValues.printNotifications();
+        printValues.printEnums();
 
-System.out.println("----------------- The Order Summary -------------------------------------------");
-        System.out.println("The purchase item is: " + generator.generatorProduct().getItem());
-        double theItemPrice = generator.generatorProduct().getPrice();
-        double itemPrice = generator.generatorProduct().getPrice();
-        System.out.println("The item price is: " + theItemPrice);
-        int quantity = generator.generatorProduct().getQuantity();
-        System.out.println("The quantity is: " + quantity + "\n");
-
-        System.out.println("Customer Payment method is: " + generator.generatorPaymentMethod().getPaymentMethod());
-        System.out.println("Customer Payment method fee is: " + generator.generatorPaymentMethod().getFee() + "\n");
-
-        System.out.println("The Shipping method is: " + generator.generatorDeliveryWay().getDeliveryWay());
-        System.out.println("The Shipping method fee is: " + generator.generatorDeliveryWay().getDeliveryWay().getExtraFee());
-        System.out.println("Your package will arrive within: " + generator.generatorDeliveryWay().getDeliveryWay().getDaysAmount() + " days.\n");
-
-        double totalPayment = payment.getTotal(theItemPrice, quantity) + generator.generatorPaymentMethod().getFee() + generator.generatorDeliveryWay().getDeliveryWay().getExtraFee();
-        System.out.println("The total payment is: " + totalPayment + "\n");
-
-System.out.println("----------------- The order shipping status -----------------------------------");
         double thePackageWeight = generator.generatorPackage().getPackageWeight();
-        // Decide what Transportation Way (Truck, Train, Ship, Airplane)
-        Transportation transportation = new Transportation();
-        System.out.println("The package shipped by " + transportation.getTransportation(thePackageWeight));
-        System.out.println(generator.generatorShippingStatus() + "\n");
+        System.out.println("The package weight is: " + thePackageWeight);
 
-System.out.println("----------------- Send Notifications to Customer ---------------------------");
-        Notifications notifications = new Notifications();
-        notifications.shipped();
-        notifications.destination();
-        notifications.tracking();
-        notifications.sendMsgStatus();
-        System.out.println("\n");
+        // Using Lambda Expression & Functional Interface to get transportation way to ship the package
+        // Decide what Transportation Way (Truck, Train, Ship, Airplane) based on the PackageWeight
+        ITransportation iTransportation = () -> {
+            String transportation = "";
+            if ((0 <= thePackageWeight) && (thePackageWeight <= 10)) {
+                transportation = "Truck";
+                printValues.printTruck();
+            } else if ((11 <= thePackageWeight) && (thePackageWeight <= 20)) {
+                transportation = "Train";
+                printValues.printTrain();
+            } else if ((21 <= thePackageWeight) && (thePackageWeight <= 30)) {
+                transportation = "The transportation way is Ship";
+                printValues.printShip();
+            } else if (31 <= thePackageWeight) {
+                transportation = "Airplane";
+                printValues.printAirplane();
+            }
+            return transportation;
+        };
 
-System.out.println("----------------- Enums Classes --------------------------------------------");
-        System.out.println("I would recommend your company to a friend: " + generator.generatorRecommendation());
-        System.out.println("My rating about the overall delivery service: " + generator.generatorDeliveryServiceRating());
-        System.out.println("My rating about the overall customer service: " + generator.generatorCustomerServiceRating() + "\n");
+        logger.error(iTransportation.transWay());
 
-System.out.println("----------------- Lambda Classes -------------------------------------------");
-        // Using Lambda to show where we can deliver or ship our products.
-        List<String> myPlaces = Places.getPlaces();
-        System.out.println("We ship to: " + myPlaces);
-        // Using Lambda to show different insurance rate.
-        List<String> myInsuranceRate = InsuranceRate.getInsuranceRate();
-        System.out.println("Our insurance rate list: " + myInsuranceRate + "$.");
-        // Using Lambda to show different discounts offers.
-        List<Double> myDiscount = Discount.getDiscount();
-        System.out.println("We hav 5%, 10%, 20%, and 30% discount: " + myDiscount + "\n");
+        // Using Lambda Expression & Functional Interface to get the Total Payment
+        double theItemPrice = generatorProduct().getPrice();
+        int quantity = generator.generatorProduct().getQuantity();
 
-System.out.println("----------------- Shipping Method Classes -------------------------------------------");
-
-//   Get Items and price from Generator --------------------
-
-
-
-
-//System.out.println("----------------------------------- M Classes -------------------------------------------");
-//    WordCount wordCount = new WordCount();
-//    System.out.println(wordCount);
+        ICart iCart = () -> {
+            double total = (theItemPrice + (theItemPrice * 0.06)) * quantity;
+            return total;
+        };
+        logger.error("The total payment is: " + iCart.totalPayment());
     }
 }
